25908859bc7148b192c7b93fdf5491a3607c0162
Natasha Montero <natashamontero@gmail.com>
2013-04-11 20:53:32 -0430

    Agregada estructura basica de rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseSubdomains::Application.config.secret_token = 'a35cafb49aa7b9d9f7972cae2a0e29942e46bdeb3698b3ca146b963f634df73383bd1e008937c80ab831f1fca29611c2725e32c29befc12dbf8ceb4845acbfe6'
