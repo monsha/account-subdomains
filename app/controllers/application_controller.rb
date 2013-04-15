2fa1c032ebda85fdcf9e646dda9c94e5bbfcd939
Natasha Montero <natashamontero@gmail.com>
2013-04-14 23:39:05 -0430

    Implementando login de usuario y redirección a su cuenta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+
   before_filter :authenticate_user!
-  
-  def index
+  before_filter :current_account
+
+  def after_sign_in_path_for(resource)
+    account_subdomain = current_user.account.subdomain
+    admin_users_url(:subdomain => account_subdomain)
   end
+
+  def is_root_domain?
+    # return true if there is no subdomain
+    result = (request.subdomains.first.present? && request.subdomains.first != "www") ? false : true
+  end
+
+  def current_account
+    # If subdomain is present, returns the account, else nil
+    if !is_root_domain?
+      @current_account ||= Account.find_by_subdomain(request.subdomains.first)
+      if @current_account.nil?
+        redirect_to root_url(:account => false, :alert => "Unknown Account/subdomain")
+      end
+    else 
+      @current_account = nil
+    end
+    @current_account
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 DeviseSubdomains::Application.routes.draw do
+  require 'domains'
+  
   devise_for :users
   constraints(Subdomain) do
-    resources :users
+    namespace :admin do
+      resources :users
+    end
   end
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +56,7 @@ DeviseSubdomains::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'application#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
